aa66a449a16e7f48ebf4ea9a41852cfcbed54834
Edson Lima <edbdelima@gmail.com>
2026-04-21 16:45:28 -0300

    first commit

---
+++ hello/hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Set properties of the default logger, including the log entry prefix and
+	// flags to disable printing the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+}
