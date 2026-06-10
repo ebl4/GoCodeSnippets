1958209beeb49658087925ba990e99a4e53fc13d
Edson Lima <edbdelima@gmail.com>
2026-06-09 23:39:49 -0300

    Add Page struct and functions for saving and loading pages

---
+++ gowiki/wiki.go
@@ -0,0 +1,19 @@
+package main
+
+import "os"
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := os.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
