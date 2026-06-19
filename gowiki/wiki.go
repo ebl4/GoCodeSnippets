9d14fcf2d09f429075f83f143ecc052ffb4de932
Edson Lima <edbdelima@gmail.com>
2026-06-19 16:02:09 -0300

    Remove unused error handling and commented code from wiki.go

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"errors"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,13 +18,6 @@ type Page struct {
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
-// func main() {
-// 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-// 	p1.save()
-// 	p2, _ := loadPage("TestPage")
-// 	fmt.Println(string(p2.Body))
-// }
-
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
@@ -38,15 +30,6 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
